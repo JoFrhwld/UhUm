3a048484feb721b73eda701385887e13520624c2
Josef Fruehwald <JoFrhwld@gmail.com>
2014-12-03 12:57:52 +0000

    better documentation

--- R/data.R
+++ R/data.R
@@ -1,15 +1,18 @@
 #' Uh and Um data from the PNC
-#' @format a data frame with 19 columns. Speech chunks were defined by 
+#' @format a data frame with 22 columns. Speech chunks were defined by 
 #' continuous speech bordered on each side by a pause of 200 miliseconds or more
 #'  \describe{
 #'    \item{idstring}{Unique id string for each speaker}
-#'    \item{word}{\code{UM} or \code{UH}}
+#'    \item{word}{\code{UM}, \code{UH}, \code{AND_UH}, \code{AND_UM}, or \code{UM_UH}}
 #'    \item{start_time}{Onset time of filled pause, in seconds}
 #'    \item{end_time}{Offset time of filled pause, in seconds}
 #'    \item{vowel_start}{Onset time of filled pause vowel}
 #'    \item{vowel_end}{Offset time of filled pause vowel}
 #'    \item{nasal_start}{Onset time of nasal, if \code{UM}}
 #'    \item{nasal_end}{Offset time of nasal, if \code{UM}}
+#'    \item{next_seg}{The next segment on the phone tier}
+#'    \item{next_seg_start}{Start time of next segment}
+#'    \item{next_seg_end}{End time of next segment}
 #'    \item{chunk_start}{Onset time of speech chunk}
 #'    \item{chunk_end}{Offset time of speech chunk}
 #'    \item{nwords}{Total number of words from this speaker}
